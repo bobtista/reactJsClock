ab6c03c2eeed537720739f21e591243702b450ce
Bobby Battista <robert.battista@gm.com>
2017-11-07 18:51:25 -0800

    exported the class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
+export default class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {date: new Date()};
